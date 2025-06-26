8b3edd4c8a9e04bd90d9933ce3078f249693d9e9
buiquanghuy2005 <151438072+buiquanghuy2005@users.noreply.github.com>
2025-06-26 00:34:33 +0000

    thread

---
+++ Quanlicauthu/complete/src/main/java/com/example/servingwebcontent/view/ClassName.java
@@ -0,0 +1,10 @@
+package com.example.servingwebcontent.view;
+
+public class ClassName extends Thread {
+ClassName cn = new ClassName();
+cn.start(); //start concurrent prog
+
+public void run (){
+//override run() of thread
+}
+}
